ec4ad4c85ee16cfac5510789982c83841f8621be
Nikita <nikita@mail.by>
2012-05-22 13:12:12 +0300

    Initial commit

---
+++ benchmark.py
@@ -0,0 +1,55 @@
+import os,logging,resource
+from nose.plugins import Plugin
+from multiprocessing import Pool
+
+log = logging.getLogger('nose.plugins.benchmark')
+
+timesResults = []
+
+def invoker(object,fname):
+    tstart = resource.getrusage(resource.RUSAGE_SELF)[0]
+    getattr(object,fname)._wrapped(object)
+    tend = resource.getrusage(resource.RUSAGE_SELF)[0]
+
+    return tend - tstart
+
+def benchmark(invocations=1, threads=1):
+    """
+    Decorator, that marks test to be executed 'invocations'
+    times using number of threads specified in 'threads'.
+    """
+    def decorator(fn):
+        global timesResults
+        def wrapper(self, *args, **kwargs):
+            pool = Pool(threads)
+            for i in xrange(invocations):
+                res = pool.apply_async(invoker, args=(self,fn.__name__))
+                # Get the results returned by invoker function
+                timesResults.append(res.get())
+
+            pool.close()
+            pool.join()
+
+        wrapper.__doc__ = fn.__doc__
+        wrapper.__name__ = fn.__name__
+        wrapper._wrapped = fn
+        return wrapper
+    return decorator
+
+class Benchmark(Plugin):
+    name = 'benchmark'
+
+    def options(self, parser, env=os.environ):
+        super(Benchmark, self).options(parser, env=env)
+
+    def configure(self, options, conf):
+        super(Benchmark, self).configure(options, conf)
+        if not self.enabled:
+            return
+
+    def afterTest(self, test):
+        # TODO:
+        # Do smth with the results
+        for i in range(len(timesResults)):
+            print '%2.60f ' % timesResults[i]
+        pass
---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+Benchmark plugin.
+
+"""
+try:
+    import ez_setup
+    ez_setup.use_setuptools()
+except ImportError:
+    pass
+
+from setuptools import setup
+
+setup(
+    name='Benchmark plugin',
+    version='0.1',
+    author='Nikita Basalaev',
+    author_email = 'nikita@mail.by',
+    description = 'Benchmark nose plugin',
+    license = 'GNU LGPL',
+    py_modules = ['benchmark'],
+    entry_points = {
+        'nose.plugins.0.10': [
+            'benchmark = benchmark:Benchmark'
+            ]
+        }
+
+    )
