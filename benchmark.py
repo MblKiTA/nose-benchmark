ca2d1354819745c3bd915374b7b080f26d18777d
Nikita <nikita@mail.by>
2012-05-23 13:20:36 +0300

    modified:   benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os, logging, resource, json, StringIO
+import os, logging, resource, json
 from nose.plugins import Plugin
 from multiprocessing import Pool
 from scipy.stats import scoreatpercentile
