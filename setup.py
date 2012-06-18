2d538454462ba470565159abc5b530a4d047abe8
Nikita Basalaev <nikita@mail.by>
2012-06-18 16:35:45 +0300

    Updated to 0.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='nose-benchmark',
-    version='0.8',
+    version='0.9',
     author='Nikita Basalaev',
     author_email = 'nikita@mail.by',
     description = 'Benchmark nose plugin',
