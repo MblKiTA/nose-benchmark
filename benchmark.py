1bcd971209b3501127cc93c68a1ad6499463eb0a
Nikita Basalaev <nikita@mail.by>
2012-06-20 14:29:54 +0300

    modified:   benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -11,44 +11,7 @@ from nose.plugins import Plugin
 try:
     from multiprocessing import Pool
 except ImportError:
-    from Queue import Queue
-    from threading import Thread
-
-    class Worker(Thread):
-        """Thread executing tasks from a given tasks queue"""
-        def __init__(self, tasks):
-            Thread.__init__(self)
-            self.tasks = tasks
-            self.daemon = True
-            self.start()
-
-        def get(self):
-            return self.result
-
-        def run(self):
-            while True:
-                func, args, kargs = self.tasks.get()
-                try:
-                    func(*args, **kargs)
-                except Exception, e:
-                    print e
-                finally:
-                    self.tasks.task_done()
-
-    class Pool:
-        """Pool of threads consuming tasks from a queue"""
-        def __init__(self, num_threads):
-            self.tasks = Queue(num_threads)
-            for _ in range(num_threads): Worker(self.tasks)
-
-        def add_task(self, func, *args, **kargs):
-            """Add a task to the queue"""
-            self.tasks.put((func, args, kargs))
-            return self.tasks
-
-        def join(self):
-            """Wait for completion of all the tasks in the queue"""
-            self.tasks.join()
+    import java.util.concurrent.ThreadPoolExecutor as Pool
 
 
 def scoreatpercentile(N, percent, key=lambda x:x):
